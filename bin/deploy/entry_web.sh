0102acace8d143afe0b75b7610def5d848f0e701
artdotlis <artdotlis@pm.me>
2025-04-16 14:18:07 +0000

    build: improve docker deployment

--- bin/deploy/entry_web.sh
+++ bin/deploy/entry_web.sh
@@ -4,7 +4,7 @@ echo "starting server - in /var/www/$APP_PRES_ROOT"
 cd "/var/www/$APP_PRES_ROOT" || exit 1
 
 for cnt in $(seq 0 $((PM2_WORKER - 1))); do
-    NODE_ENV=production HOST="$NODE_HOST" PORT="$((NODE_PORT + cnt))" \
+    NODE_ENV=production PORT="$((NODE_PORT + cnt))" \
         pm2 start --interpreter bun serve --directory "$APP_PRES_PUB_ROOT" -n "server-$cnt"
 done
 
